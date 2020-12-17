3805b17727febccdb6c7f3d5b8721640962d3684
黄高峰 <15879831437@163.com>
2020-12-17 23:03:02 +0800

    1.使用alpinejs魔术助手$truncate和$scroll

--- js/demo4.js
+++ js/demo4.js
@@ -12,6 +12,9 @@ function demo4Data() {
         url: 'https://jsonplaceholder.typicode.com/todos/1',
         autoTimer: 1000,
         autoIntervalTest: false,
+        characters: 50, 
+        count: 5,
+        string: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         funtionToRun() {
             console.log('Hello console')
         },
