ae00a385e2c53aa36109fe39f911435dabd18f4d
HGF <15879831437@163.com>
2021-03-05 12:01:05 +0800

    1.更新alpine.js V2.8.1 Fixed

--- js/demo5.js
+++ js/demo5.js
@@ -2,6 +2,24 @@ function initData () {
 	return {
 		maxSeats: 3,
         seats: [],
+        open: false,
+        showModal: false,
+        trigger: {
+            ['@click']() {
+                this.open = true
+            },
+        },
+        dialogue: {
+            ['x-show']() {
+                return this.open
+            },
+            ['@click.away']() {
+                this.open = false
+            },
+            ['x-text']() {
+                return 'gaofeng'
+            }
+        }, 
         availableSeats() {
             return this.maxSeats - this.seats.length;
         },
